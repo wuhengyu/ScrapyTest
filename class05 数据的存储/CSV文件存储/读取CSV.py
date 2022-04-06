5f83e6b3d3d7e20a6cb1794cbb43c21a044cb054
吴恒宇 <admin@example.com>
2022-04-06 15:43:34 +0800

    class5 file update

--- class05 数据的存储/CSV文件存储/读取CSV.py
+++ class05 数据的存储/CSV文件存储/读取CSV.py
@@ -16,3 +16,15 @@ import pandas
 
 df = pandas.read_csv('write_csv_data.csv')
 print(df)
+
+print(f'{"==" * 50}')
+# 转化列表或元组
+df = pandas.read_csv('write_csv_data.csv')
+data3 = df.values.tolist()
+print(data3)
+
+print(f'{"==" * 50}')
+# 直接对df遍历获取列表
+df = pandas.read_csv('write_csv_data.csv')
+for index, row in df.iterrows():
+    print(row.tolist())
